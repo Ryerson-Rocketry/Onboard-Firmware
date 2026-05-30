11ffad3d84f3219189be580b491ce613bb74a4c7
Sowhum <sohamnaithani@gmail.com>
2026-05-29 21:24:57 -0400

    Initial commit, cleaned current files

--- src/helpers.h
+++ src/helpers.h
@@ -3,34 +3,57 @@
 
 
 ////    Includes    ////
+#include "rrc_encoder/src/rrc_encoder.h"
 #include <Arduino.h>
+#include <SPI.h> 
 #include <gps.h>
 #include <ms5611.h>
 #include <MPU6050.h> 
+#include <Adafruit_INA260.h>
 #include <SD.h>
-#include "rrc_encoder/src/rrc_encoder.h"
+#include <RH_RF95.h> 
 
 
 ////    Defines    ////
 
-#define RFD_BAUD      57600
-#define RFD_SERIAL    Serial2
-#define GPS_SERIAL    Serial7
-#define BARO_WIRE     Wire2
-#define BUZZER        PIN_A12
-#define BUZZER_ENABLE PIN_A13//always apply
-#define IMU_WIRE      Wire1
-#define AD0_VAL        0
-#define SERIAL_MONITOR_BAUD 115200
-#define PIN_RED        4
-#define PIN_GREEN      3
-#define PIN_BLUE       2                                                                                                                         
+#define GPS_SERIAL              Serial7
+#define BARO_WIRE               Wire     
+#define AD0_VAL                 0
+#define SERIAL_MONITOR_BAUD     115200
+#define PIN_RED                 4
+#define PIN_GREEN               3
+#define PIN_BLUE                2       
+#define SD_CS_PIN               10
+#define RFM96_CS                9
+#define RFM96_GPIO              5
+#define RF96_FREQ               433.0
+#define BUZZER                  PIN_A6
+#define BUZZER_ENABLE           PIN_A7
 
-long freq = 1000;
-bool led_debug = false;
 
 ////    Constants    ////
-String logFileName = "log.txt";
+
+long freq               = 1000;
+bool led_debug          = false;
+bool built_in_SD_card   = true;
+int  acc_range          = 1; 
+int  gyro_range         = 1; 
+String logFileName      = "log.txt";
+double                     temp;
+double                     pres;
+double                     lon;
+double                     lat;
+double                     volt_battery;
+float                      x;
+float                      y;
+float                      z;
+float                      r;
+AStruct                    imu_acc;
+GStruct                    imu_gyro;
+uint32_t                   start;
+uint8_t                    counter = 0;
+
+
 /*
 
 const char outputFormat[] =
@@ -44,41 +67,55 @@ Location:    %lf, %lf
 )""";
 */
 
+//Update May 20: Added spaces cause it was AIDS to read
 const char outputFormat[] =
-    R"""(%lu,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf
+    R"""(%lu, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf
 )""";
 
+
 ////    Objects    ////
-Ms5611      baro;
-GPS         gps;
+
+Ms5611              baro;
+GPS                 gps;
+RH_RF95             rf96(RFM96_CS, RFM96_GPIO);
+MPU                 mpu;
+Adafruit_INA260     ina260;
+SDClass             sd;
+
 struct
 {
     bool    baro   = false;
     bool    gps    = false;
     bool    sdcard = false;
-    bool    rfd    = false;
+    bool    mpu    = false;
+    bool    ina    = false;
+    bool    lora   = false; 
 } partsStates;
 
-////    Functions declairations    ////
-void transmit   (double data, uint8_t header, uint32_t time);
-void debug      (void);
+enum initStatus
+{
+    FAULT_BARO = (1 << 0),
+    FAULT_GPS  = (1 << 1),
+    FAULT_SD   = (1 << 2),
+    FAULT_INA  = (1 << 3),
+    FAULT_IMU  = (1 << 4),
+
+    DATA_BARO =  (1 << 5),
+    DATA_GPS  =  (1 << 6),
+    DATA_SD   =  (1 << 7),
+    DATA_INA  =  (1 << 8),
+    DATA_IMU  =  (1 << 9),
+};
+
+uint32_t status = 0;
+
+
+////    Functions declarations    ////
 void buzzFor    (unsigned int time_ms, unsigned int after = 0);
 void setParts   (void);
 
+
 ////    Functions definitions    ////
-void transmit(double data, uint8_t header, uint32_t time)
-{
-    uint8_t  package[10] = {0};
-    encode(data, header, time, package);
-    RFD_SERIAL.write(package, 10);
-}
-
-
-void debug(void)
-{
-    static int count = 0;
-    Serial.printf("debug    %d\n", count++);
-}
 
 void buzzFor(unsigned int time_ms, unsigned int after)
 {
@@ -93,124 +130,88 @@ void buzzFor(unsigned int time_ms, unsigned int after)
 }
 
 
-/*
-void errorLED(int errorcode)
-{
-  unsigned long time = 0;
-  unsigned long delta = 0;
-  time = millis();
-  
-  switch(errorcode) {
-    case 1:      
-      analogWrite(PIN_RED,   255);
-      analogWrite(PIN_GREEN, 0);
-      analogWrite(PIN_BLUE,  0);
-      break;
-  
-    case 2:
-
-      analogWrite(PIN_RED,   0);
-      analogWrite(PIN_GREEN, 255);
-      analogWrite(PIN_BLUE,  0);
-      break;
-
-    case 3:
-
-      analogWrite(PIN_RED,   0);
-      analogWrite(PIN_GREEN, 0);
-      analogWrite(PIN_BLUE,  255);
-      break;
-
-    case 4:
-
-      analogWrite(PIN_RED,   0);
-      analogWrite(PIN_GREEN, 200);
-      analogWrite(PIN_BLUE,  255);
-
-      break;
-    
-    case 5:
-
-      analogWrite(PIN_RED,   200);
-      analogWrite(PIN_GREEN, 12);
-      analogWrite(PIN_BLUE,  0);
-
-      break;
-      case 6:
-            analogWrite(PIN_RED,   0);
-
- analogWrite(PIN_GREEN, 0);
-      analogWrite(PIN_BLUE,  0);
-
-      break;
-      
-
-  }
-}*/
-
 void setParts(void)
 {
+
     // init MS5611
     if(!partsStates.baro)
-        {        
+    {        
         if(baro.init(&BARO_WIRE)){
-
             partsStates.baro = false;
             Serial.println("MS5611 init error");
-           // buzzFor(500, 50);
-            buzzFor(50, 50);
-            //buzzFor(50, 250);
+            status |= FAULT_BARO;
         }
         else
         {
             partsStates.baro = true;
+            status &= ~FAULT_BARO;
             Serial.println("MS5611 init OK");
-            //buzzFor(50, 100);
         }
     }
 
     // init GPS
     if(!partsStates.gps)
     {
-        gps.init(&GPS_SERIAL, 9600);
-        partsStates.gps = true;
-        Serial.println("GPS init OK");
-        //buzzFor(50, 100);
+        if(gps.init(&GPS_SERIAL, 9600)){
+            partsStates.gps = false;
+            Serial.println("GPS init error");
+            status |= FAULT_GPS;
+        }
+        else{
+            partsStates.gps = true;
+            status &= ~FAULT_GPS;
+            Serial.println("GPS init OK");
+        }    
     }
-
-
-    // init RFD
-    if(!partsStates.rfd)
-    {
-        RFD_SERIAL.begin(RFD_BAUD);
-        partsStates.rfd = true;
-        Serial.println("RFD init OK");
-        //buzzFor(50, 100);
-    }
-
 
     // init SD card
     if(!partsStates.sdcard)
     {
-        if(!SD.begin(BUILTIN_SDCARD))
+        if (!sd.begin(SD_CS_PIN))
         {
             partsStates.sdcard = false;
             Serial.println("SD Card init error");
-           // buzzFor(500, 50);
-           // buzzFor(50, 50);
-           // buzzFor(50, 50);
-           // buzzFor(50, 250);
-
+            status |= FAULT_SD;
         }
         else
         {
             partsStates.sdcard = true;
+            status &= ~FAULT_SD;
             Serial.println("SD Card init OK");
-           // buzzFor(50, 100);
+        }
+    }
+
+    // init INA
+    if(!partsStates.ina)
+    {        
+        if(!ina260.begin()){
+            partsStates.ina = false;
+            Serial.println("INA260 init error");
+            status |= FAULT_INA;
+        }
+        else
+        {
+            partsStates.ina = true;
+            status &= ~FAULT_INA;
+            Serial.println("INA260 init OK");
+        }
+    }
+
+    // init MPU
+    if(!partsStates.mpu)
+    {        
+        if(!mpu.init(gyro_range,acc_range)){
+            partsStates.mpu = false;
+            Serial.println("MPU6050 init error");
+            status |= FAULT_IMU;
+        }
+        else
+        {
+            partsStates.mpu = true;
+            status &= ~FAULT_IMU;
+            Serial.println("MPU6050 init OK");
         }
     }
 }
 
-
-
-#endif  
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,21 @@
 ////    Includes    ////
+#include "helpers.h"
 #include <Arduino.h>
 #include <math.h>
 #include <Wire.h>
-#include "helpers.h"
-#include <Adafruit_INA260.h>
-bool rfd_comms_ini = true;
+#include <SPI.h> 
+#include <RH_RF95.h> 
 
-double temp, pres, lon, lat;
-// float PDOP, VDOP, HDOP;
-MPU mpu;
-AStruct imu_acc;
-Adafruit_INA260 ina260 = Adafruit_INA260();
-
-float x, y, z, r = 0;
-uint32_t start;
-uint8_t counter = 0;
 ////    Initilization setup    ////
 void setup(void)
 {
-
-    pinMode(BUZZER, OUTPUT);
-    pinMode(BUZZER_ENABLE, INPUT_PULLUP);
-
-
+    SPI.begin();
     Wire.begin();
-    Wire1.begin();
-    Wire2.begin();
-    ina260.begin();
-    mpu.pwr_setup();
-    mpu.acc_setup(1);
-  
-    if (!ina260.begin())
-    {
-        buzzFor(100, 100);
-    }
-
-    // start serial monitor
     Serial.begin(SERIAL_MONITOR_BAUD);
-
+    
     setParts();
-    pinMode(34, OUTPUT);
-    digitalWrite(34, LOW);
 
-    // basically rfd uses the most power & since rocket is going to be idle on platorm for awhile dont do anything until bit is sent
-    /*
-        while (true)
-        {
-            // RFD_SERIAL.printf("idle\n");
-            if (RFD_SERIAL.available())
-            {
-                String command = RFD_SERIAL.readStringUntil('\n');
-                command.toLowerCase();
-
-                if (command.equals("launch"))
-                {
-                    break;
-                }
-                else
-                {
-                    Serial.printf("command \"%s\" unrecognized\n", command.c_str());
-                }
-            }
-        }
-        */
-
-    Serial.printf("launching\n");
+    Serial.printf("Setup Done\n");
 }
 
 ////    Main loop    ////
@@ -72,54 +23,63 @@ void loop(void)
 {
     static uint32_t timestamp = 0;
     char string[256] = {0};
-    start = millis(); // store current time
+    start = millis(); 
 
     setParts();
-    float volt_battery = ina260.readBusVoltage();
 
-    // read MS5611
+    // harvest data; check data quality with partstates
+
     if (partsStates.baro)
     {
         if (baro.getTempPress(&temp, &pres))
         {
             Serial.printf("baro read failed\n");
-            buzzFor(250, 250);
+            status |= DATA_BARO;
+        }
+        else{
+            status &= ~DATA_BARO;
         }
     }
-
-    if (mpu.getErr())
-    {
-        mpu.pwr_setup();
-        mpu.acc_setup(1);
-        Serial.printf("imu read failed\n");
-        buzzFor(20, 20);
-
-        mpu.get_acc(1, &imu_acc);
-    }
-
-    else
-    {
-        mpu.get_acc(1, &imu_acc);
-    }
-
-    int numbSat, quality;
-    char opMode;
-    float HDOP, PDOP, sigStrength;
-    // read gps
 
     if (partsStates.gps)
     {
         if (gps.read_RMC(&lon, &lat, 4000))
         {
             Serial.printf("gps RMC read failed\n");
-            buzzFor(250, 250);
+            status |= DATA_GPS;
+        }
+        else{
+            status &= ~DATA_GPS;
         }
     }
 
-    // print stuff to serial and SD card (need to call array for xyz, use equation for r here)
+    if (partsStates.ina)
+    {
+        if (volt_battery = ina260.readBusVoltage(); volt_battery >100)
+        {
+            Serial.printf("ina read failed\n");
+            status |= DATA_INA;
+        }
+        else{
+            status &= ~DATA_INA;
+        }
+    }
+
+    if (partsStates.mpu)
+    {
+        if (mpu.get_acc(acc_range, &imu_acc) or mpu.get_gyro(gyro_range, &imu_gyro))
+        {
+            Serial.printf("mpu read failed\n");
+            status |= DATA_IMU;
+        }
+        else{
+            status &= ~DATA_IMU;
+        }
+    }
+
     sprintf(
         string, outputFormat,
-        timestamp++, volt_battery, imu_acc.XAxis, imu_acc.YAxis, imu_acc.ZAxis, temp, pres, lat, lon);
+        timestamp++, volt_battery, imu_acc.XAxis, imu_acc.YAxis, imu_acc.ZAxis, imu_gyro.XAxis, imu_gyro.YAxis, imu_gyro.ZAxis, temp, pres, lat, lon);
 
     Serial.printf("%s", string);
 
@@ -130,61 +90,14 @@ void loop(void)
         if (dataFile)
         {
             dataFile.println(string);
+            status |= DATA_SD;
             dataFile.close();
         }
         else
         {
             Serial.printf("error opening %s\n", logFileName.c_str());
             partsStates.sdcard = false;
-            buzzFor(100, 20);
+            status &= ~DATA_SD;
         }
-    }
-
-    // encode and transmit data
-    rfd_comms_ini = false;
-
-    if (rfd_comms_ini == true)
-    {
-
-        String command = RFD_SERIAL.readStringUntil('\n');
-        command.toLowerCase();
-        if (command.equals("launch"))
-        {
-            rfd_comms_ini = false;
-            Serial.printf("launching\n");
-        }
-        else
-        {
-            if (counter == 20) // triggers every 20seconds
-            {
-                RFD_SERIAL.printf("idle\n");
-            }
-            else
-            {
-                counter = 0; // reset the counter
-                delay(1000);
-            }
-            counter++;
-            Serial.printf("command \"%s\" unrecognized %d \n", command.c_str(), counter);
-        }
-    }
-    /*
-        else
-        {
-            RFD_SERIAL.println(string);
-            Serial.println("transmitting");
-        }
-
-        }*/
-    /**/
-    else
-    {
-
-        RFD_SERIAL.printf(string);
-        Serial.println("transmitting");
-    }
-
-    while (millis() - start <= 4000) // print every 4 second
-    {
     }
 }
